c132e86a9a2dd7e0577aa157c3d1dea1dedd325f
Hamza28Salhi <91228154+Hamza28Salhi@users.noreply.github.com>
2023-02-17 19:09:14 +0100

    cance button

---
+++ migrations/Version20230217061905.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230217061905 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE equipe ADD picture VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE matches CHANGE score score VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE equipe DROP picture');
+        $this->addSql('ALTER TABLE matches CHANGE score score VARCHAR(255) NOT NULL');
+    }
+}
